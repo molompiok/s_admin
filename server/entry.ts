27d25f57fa3f611f1989d54d88e385b913f66442
simeon619 <sijean619@gmail.com>
2026-01-02 09:54:32 +0000

    Implement Scaling/Monitoring in s_admin

--- server/entry.ts
+++ server/entry.ts
@@ -1,22 +1,58 @@
 import { createTodoHandler } from "./create-todo-handler";
 import { apply, serve } from "@photonjs/express";
 import express from "express";
+import { closeQueue, getServerQueue } from "../api/Scalling/bullmqClient.js";
+import { LoadMonitorService } from "../api/Scalling/loadMonitorClient.js";
+import logger from "../api/Logger.js";
 
 const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3008;
+const SERVICE_ID = process.env.SERVICE_ID;
 
 export default startApp() as unknown;
 
-function startApp() {
+async function startApp() {
   const app = express();
-  
+
   app.get('/health', async (_req, res) => {
-    res.status(200).json({ok:true});
+    res.status(200).json({ ok: true });
     return
   });
-  
+
   apply(app, [createTodoHandler]);
 
-  return serve(app, {
+  import { Server } from "http";
+
+  // ...
+
+  const server = (await serve(app, {
     port,
+  })) as unknown as Server;
+
+  const loadMonitoring = new LoadMonitorService({
+    bullmqQueue: getServerQueue(),
+    logger: logger,
+    serviceId: SERVICE_ID || 's_admin',
+    serviceType: 'app',
   });
+
+  loadMonitoring.startMonitoring()
+  const shutdown = async () => {
+    console.log(`[Admin Server ${SERVICE_ID}] Arrêt demandé...`);
+    server.close(async () => {
+      console.log(`[Admin Server ${SERVICE_ID}] Serveur HTTP fermé.`);
+      await closeQueue(); // Fermer la connexion BullMQ/Redis
+      process.exit(0);
+    });
+    // Forcer la fermeture après un délai si le serveur ne se ferme pas
+    setTimeout(async () => {
+      console.error(`[Admin Server ${SERVICE_ID}] Arrêt forcé après timeout.`);
+      await closeQueue();
+      process.exit(1);
+    }, 10000); // 10 secondes timeout
+  };
+
+  process.on('SIGTERM', shutdown);
+  process.on('SIGINT', shutdown);
+
+  return server;
 }
