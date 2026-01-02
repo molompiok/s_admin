9800ef8acae4f73643fbe4e8ddf9d659fce6a76c
simeon619 <sijean619@gmail.com>
2026-01-02 09:34:35 +0000

    save

--- server/entry.ts
+++ server/entry.ts
@@ -2,13 +2,18 @@ import { createTodoHandler } from "./create-todo-handler";
 import { apply, serve } from "@photonjs/express";
 import express from "express";
 
-const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
+const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3008;
 
 export default startApp() as unknown;
 
 function startApp() {
   const app = express();
-
+  
+  app.get('/health', async (_req, res) => {
+    res.status(200).json({ok:true});
+    return
+  });
+  
   apply(app, [createTodoHandler]);
 
   return serve(app, {
